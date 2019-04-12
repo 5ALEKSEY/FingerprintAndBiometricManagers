d305f6e774aa02ebac5cab7f6d9520abcdeedd74
Aliaksei <a.kuzmenko@vironit.co.uk>
2019-04-12 13:54:17 +0300

    disable android x

--- app/src/main/java/com/example/fingerprintandbiometric/extensions/ActivityExtensions.kt
+++ app/src/main/java/com/example/fingerprintandbiometric/extensions/ActivityExtensions.kt
@@ -1,8 +1,8 @@
 package com.example.fingerprintandbiometric.extensions
 
-import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
 
-fun Activity.notifyUser(message: String, duration: Int = Toast.LENGTH_SHORT) {
+fun AppCompatActivity.notifyUser(message: String, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this.applicationContext, message, duration).show()
 }
