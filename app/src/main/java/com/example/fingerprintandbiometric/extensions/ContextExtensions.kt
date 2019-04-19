4d657ba1cbf16951dfed18b207d72f031d41ad81
Aliaksei <a.kuzmenko@vironit.co.uk>
2019-04-19 12:08:47 +0300

    refactor fingerprint state enum class

--- app/src/main/java/com/example/fingerprintandbiometric/extensions/ContextExtensions.kt
+++ app/src/main/java/com/example/fingerprintandbiometric/extensions/ContextExtensions.kt
@@ -9,14 +9,12 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 
-enum class FingerprintState(private val stateMessage: String) {
+enum class FingerprintState(val stateMessage: String) {
     FINGERPRINT_ALLOW("Everything will be nice :)"),
     VERSION_DOES_NOT_ALLOW("Your device version doesn't allow for fingerprint"),
     DEVICE_HARDWARE_DOES_NOT_ALLOW("Your device hasn't hardware for fingerprint"),
     NO_ENROLLED_FINGERPRINTS("Please, add fingerprint for device in security settings"),
     UNKNOWN_STATE("Ooooops, wtf");
-
-    fun getStateMessage() = stateMessage
 }
 
 fun Context.checkDeviceFingerprintState() =
